8dfb1e570c358170f2f12448d860b136e39edaaf
elmehdimst <mehdi.moustahib@outlook.com>
2023-04-08 16:09:02 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,16 +15,16 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "mstprocess"
 
     workspaces {
-      name = "learn-terraform-github-actions"
+      name = "terraform-github-actions"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "random_pet" "sg" {}
