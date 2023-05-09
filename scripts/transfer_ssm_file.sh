b52c4670ddb4f959fcc06a6fef41616a0dd63295
elmehdimst <elmehdi.moustahib.external@axa.com>
2023-05-09 10:10:37 +0100

    update

---
+++ scripts/transfer_ssm_file.sh
@@ -0,0 +1,19 @@
+# This script will explain how to transfer a file to EC2 using SSM ONLY!
+
+# You will need to have permission to run SSM commands on the target machine and have sudo access as well
+
+# Infos
+INSTANCE_ID=i-0a0f96620f86efabc
+FILE_NAME=oracle.sh
+
+# Step 1: Run command on machine to install netcat and dump from port to filename
+# < Start session
+aws ssm start-session --target $INSTANCE_ID --document-name
+# < (On target machine) :
+cd && sudo yum install nc -y && sudo nc -l -p 1234 > the_file.tar.gz
+
+# Step 2: On another shell, open a port-forwarding session from your machine to the target machine
+aws ssm start-session --target $INSTANCE_ID --document-name AWS-StartPortForwardingSession --parameters '{"portNumber":["1234"],"localPortNumber":["1234"]}'
+
+# Step 3: On yet another shell, cat the source file into the transfer port on localhost over the tunnel
+nc -w 3 127.0.0.1 1234 < the_file.tar.gz
